b3536cbb77a48d046dc969fe6d3c2c9b009f6d6e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-01-25 22:41:41 -0300

    test: remove dollar side effects

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -3,7 +3,9 @@ import { Dollar } from '../src/dollar'
 describe('Dollar', () => {
   it('should return 10 when 5 dollars is multiplied by 2', () => {
     const five = new Dollar(5)
-    five.times(2)
-    expect(five.amount).toBe(10)
+    let product: Dollar = five.times(2)
+    expect(product.amount).toBe(10)
+    product = five.times(3)
+    expect(product.amount).toBe(15)
   })
 })
